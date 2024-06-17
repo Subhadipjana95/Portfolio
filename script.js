96eed2a2da95b39b8e51f4de81e8bdcdcc3f3329
Subhadip <codesubhadip95@gmail.com>
2024-06-17 23:30:27 +0530

    4th Commit

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+ var elemC = document.querySelector("#elem-container")
+ var fixed = document.querySelector("#fixed-images")
+ 
+ elemC.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
