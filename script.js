49e270072958a134f30ceadf62a168e925f17a60
Subhadip <codesubhadip95@gmail.com>
2024-06-20 22:34:31 +0530

    10th Commit

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-
- var elemC = document.querySelector("#elem-container")
- var fixed = document.querySelector("#fixed-images")
- 
- elemC.addEventListener("mouseenter",function(){
-    fixed.style.display = "block"
-})
-elemC.addEventListener("mouseleave",function(){
-    fixed.style.display = "none"
-})
-
-var elems = document.querySelectorAll(".elem")
-elems.forEach(function(e){
-    e.addEventListener("mouseenter",function(){
-        var image = e.getAttribute("data-image")
-        fixed.style.backgroundImage = `url(${image})`
+function page3Animation(){
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-images")
+    
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
     })
-})
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+    
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+    });
+}
+
+page3Animation()
+swiperAnimation()
+    
