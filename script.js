ceb04db9c00e1bac0710dd0b707d3a918102f931
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2024-06-22 00:34:23 +0530

    Update script.js
    12th Commit

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ menu.addEventListener("click", function(){
         navImg.style.opacity = 0
         flag = 1
     }else {
-        fullScr.style.top = "-110%"
+        fullScr.style.top = "-115%"
         navImg.style.opacity = 1
         flag = 0
     }
